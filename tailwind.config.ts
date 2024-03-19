cff0f801bba87535419c95fa1464d213aa059ed4
Sapphyra Wiser <sapphybara@gmail.com>
2024-03-18 21:35:31 -0600

    setup tailwind and nextui

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+// tailwind.config.js
+import { nextui } from "@nextui-org/react";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+};
