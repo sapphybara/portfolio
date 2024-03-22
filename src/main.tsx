ebe372e4ad4198e37486c5f37bd7de9ee2d42237
Sapphyra Wiser <sapphybara@gmail.com>
2024-03-22 16:39:29 -0600

    added CssBaseline and routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.tsx";
 import "@fontsource/lato/300.css";
 import "@fontsource/lato/400.css";
 import "@fontsource/lato/700.css";
 import "@fontsource/lato/900.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "", element: <p>Home</p> },
+      { path: "portfolio", element: <p>Portfolio</p> },
+      { path: "contact", element: <p>Contact</p> },
+    ],
+  },
+]);
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
