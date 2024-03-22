5090a693c8f94852d475a030536e9130080b3a03
Sapphyra Wiser <sapphybara@gmail.com>
2024-03-22 17:13:42 -0600

    customize links for theme

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,6 +1,20 @@
 import { createTheme } from "@mui/material";
+import { LinkProps } from "@mui/material/Link";
+import LinkBehavior from "./LinkBehavior";
 
 const theme = createTheme({
+  components: {
+    MuiLink: {
+      defaultProps: {
+        component: LinkBehavior,
+      } as LinkProps,
+    },
+    MuiButtonBase: {
+      defaultProps: {
+        LinkComponent: LinkBehavior,
+      },
+    },
+  },
   typography: {
     fontFamily: [
       "Lato", // Specify Lato as the primary font
