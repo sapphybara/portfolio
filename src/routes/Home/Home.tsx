af6835e7840ef767ce04a53ab126903d2417bbfe
Sapphyra Wiser <sapphybara@gmail.com>
2024-03-25 15:16:42 -0600

    add content and styling to home page

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,20 +1,46 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Link, Typography } from "@mui/material";
 import "./home.css";
 
 const Home = () => {
   return (
-    <section className="mt-8 mx-8 home">
-      <Box className="header">
-        <Typography>hi, my name is</Typography>
-        <Typography variant="h1">Sapphyra</Typography>
+    <section className="mt-[4rem] ml-8 home">
+      <Box className="header flex flex-wrap">
+        <Typography className="w-full" color="secondary.main">
+          hi, my name is
+        </Typography>
+        <Box className="flex flex-wrap justify-between w-4/5">
+          <Typography variant="h1">Sapphyra</Typography>
+          <Box
+            className="rounded-[50%]"
+            sx={{ bgcolor: "text.secondary", height: "100px", width: "100px" }}
+          >
+            <img
+              className="rounded-[50%]"
+              src="src/assets/img.png"
+              alt="Picture of Sapphyra Wiser"
+              width="100px"
+              height="100px"
+            />
+          </Box>
+        </Box>
       </Box>
-      <Typography className="about mt-[6rem] ml-4">
-        I'm Sapphyra, a passionate Front End Developer adept in React,
-        JavaScript, TypeScript, and UI/UX design. My experience at Pacific
-        Northwest National Lab included leading projects, enhancing UI/UX for
-        increased funding, and ensuring accessibility. I thrive on
-        problem-solving, mentorship, and bringing ideas to life through
-        innovative digital experiences.
+      <Box className="header group my-12">
+        <Typography className="decorator" color="secondary.main">
+          I am a
+        </Typography>
+        <Typography variant="h2">Developer</Typography>
+        <Typography className="amp" color="secondary.main">
+          &
+        </Typography>
+        <Typography className="design" variant="h2">
+          Designer
+        </Typography>
+      </Box>
+      <Typography className="about mt-16 mx-4">
+        I build diverse front end and mobile applications, and my passion lies
+        in accessibility, in every domain.&nbsp;
+        <Link href="/portfolio">Check out my work</Link> or&nbsp;
+        <Link href="/contact">get in touch</Link>!
       </Typography>
     </section>
   );
