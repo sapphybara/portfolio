a05e7883573f938de7f6b41e2f77558477a0314a
Sapphyra Wiser <sapphybara@gmail.com>
2024-03-26 15:47:18 -0600

    updated content of the /portfolio route

--- src/routes/Portfolio.tsx
+++ src/routes/Portfolio.tsx
@@ -1,11 +1,23 @@
-import { Typography } from "@mui/material";
+import { Box, Link, Typography } from "@mui/material";
 
 const Portfolio = () => {
   return (
-    <section>
-      <Typography variant="h2">Portfolio</Typography>
-      <Typography>I am a talented girly who can do many things!!</Typography>
-    </section>
+    <Box component="section" className="mt-16 mx-8">
+      <Typography variant="decoration">Check out my</Typography>
+      <Typography variant="h1">Portfolio</Typography>
+      <Box className="mt-12">
+        <Typography variant="h2">Overview</Typography>
+        <Typography paragraph>
+          As a frontend developer and designer, I bring a versatile skill set
+          that combines technical expertise with a keen eye for aesthetics and
+          user experience.
+        </Typography>
+        <Typography paragraph>
+          To view more about the individual skills and experience I have,&nbsp;
+          <Link href="/resume">click here</Link>.
+        </Typography>
+      </Box>
+    </Box>
   );
 };
 
