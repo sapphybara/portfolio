e8eb469967adb029f0d3b55d81bf27e9dd893e2a
Sapphyra Wiser <sapphybara@gmail.com>
2024-03-26 15:27:49 -0600

    allow underscored, unused variables eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,5 +14,15 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    // note you must disable the base rule as it can report incorrect errors
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        "argsIgnorePattern": "^_",
+        "varsIgnorePattern": "^_",
+        "caughtErrorsIgnorePattern": "^_"
+      }
+    ]
   },
 }
