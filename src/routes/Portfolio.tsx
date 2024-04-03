290c259d7950c5e9386d687cd658cf423cd017a4
Sapphyra Wiser <sapphybara@gmail.com>
2024-04-03 16:34:23 -0600

    WIP adding content to portfolio route

--- src/routes/Portfolio.tsx
+++ src/routes/Portfolio.tsx
@@ -1,15 +1,33 @@
-import { Box, Link, Typography } from "@mui/material";
+import {
+  Box,
+  Card,
+  CardContent,
+  CardHeader,
+  Link,
+  Typography,
+} from "@mui/material";
+
+const portfolioItems = [
+  {
+    title: "HySCAN",
+    subtitle: "Hydrogen Safety Codes & Standards Applicability Navigator",
+    affiliation: "Pacific Northwest National Laboratory",
+    description:
+      "How do I as a developer support those who utilize hydrogen (H2) in identifying relevant codes and standards for their systems? How do we maintain up-to-date compliance? HySCAN, part of H2Tools, simplifies compliance with intuitive workflows. Developed with ReactJS and styled-components, it swiftly identifies areas of compliance and categorizes questions, empowering users with tailored solutions.",
+    techStack: ["ReactJS", "styled-components", "Figma"],
+  },
+];
 
 const Portfolio = () => {
   return (
-    <Box component="section">
+    <Box className="w-full" component="section">
       <Typography variant="decoration">Check out my</Typography>
       <Typography className="decorated" variant="h1">
         Portfolio
       </Typography>
-      <Box className="mt-12">
+      <Box className="mt-12 max-w-xl">
         <Typography variant="h2">Overview</Typography>
-        <Typography paragraph>
+        <Typography className="mt-8" paragraph>
           As a frontend developer and designer, I bring a versatile skill set
           that combines technical expertise with a keen eye for aesthetics and
           user experience.
@@ -19,6 +37,27 @@ const Portfolio = () => {
           <Link href="/resume">click here</Link>.
         </Typography>
       </Box>
+      {portfolioItems.map(
+        ({ title, subtitle, affiliation, description, techStack }) => (
+          <Card className="mb-4" key={title}>
+            <CardHeader
+              className="mx-4"
+              title={title}
+              titleTypographyProps={{ variant: "h3", component: "h3" }}
+              subheader={subtitle}
+              subheaderTypographyProps={{ variant: "h4", component: "h4" }}
+            />
+            <CardContent className="mx-8">
+              <Typography variant="h5">Description</Typography>
+              <Typography paragraph>{description}</Typography>
+              <Typography variant="h5">Affiliation</Typography>
+              <Typography paragraph>{affiliation}</Typography>
+              <Typography variant="h5">Tech Stack</Typography>
+              <Typography paragraph>{techStack.join(", ")}</Typography>
+            </CardContent>
+          </Card>
+        )
+      )}
     </Box>
   );
 };
