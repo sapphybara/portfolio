962ed5efdf017e8e49b6731afdb1d5b345bf1867
Sapphyra Wiser <sapphybara@gmail.com>
2024-04-08 19:20:39 -0600

    redo homepage styling

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -1,32 +1,31 @@
-import { Box, Link, Typography } from "@mui/material";
+import { Box, Typography, styled } from "@mui/material";
 import "./home.css";
 
 const Home = () => {
+  const ProfilePicture = styled("img")((props) => ({
+    backgroundColor: props.theme.palette.text.secondary,
+    borderRadius: "50%",
+    width: "100px",
+    height: "100px",
+  }));
+
   return (
     <Box component="section">
       <Box className="header flex flex-wrap">
-        <Typography className="w-full" variant="decoration">
-          hi, my name is
-        </Typography>
         <Box className="flex flex-wrap justify-between w-11/12">
-          <Typography className="decorated" variant="h1">
-            Sapphyra
-          </Typography>
-          <Box
-            className="rounded-[50%] w-[100px] h-[100px]"
-            bgcolor="text.secondary"
-          >
-            <img
-              className="rounded-[50%]"
-              src="src/assets/img.png"
-              alt="Picture of Sapphyra Wiser"
-              width="100px"
-              height="100px"
-            />
+          <Box>
+            <Typography variant="decoration">hi, my name is</Typography>
+            <Typography className="decorated" variant="h1">
+              Sapphyra
+            </Typography>
           </Box>
+          <ProfilePicture
+            src="src/assets/img.png"
+            alt="Picture of Sapphyra Wiser"
+          />
         </Box>
       </Box>
-      <Box className="header flex flex-wrap items-center my-12">
+      <Box className="header grid">
         <Typography className="decorator grow w-full" variant="decoration">
           I am a
         </Typography>
@@ -42,9 +41,7 @@ const Home = () => {
       </Box>
       <Typography className="about mt-16 mx-4">
         I build diverse front end and mobile applications, and my passion lies
-        in accessibility, in every domain.&nbsp;
-        <Link href="/portfolio">Check out my work</Link> or&nbsp;
-        <Link href="/contact">get in touch</Link>!
+        in accessibility, in every domain.
       </Typography>
     </Box>
   );
