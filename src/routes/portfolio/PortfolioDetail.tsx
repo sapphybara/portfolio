b0c7e4963f007e76676f98d986e8adee946f69f2
Sapphyra Wiser <sapphybara@gmail.com>
2024-04-18 18:27:39 -0600

    add not found and back button elements

--- src/routes/portfolio/PortfolioDetail.tsx
+++ src/routes/portfolio/PortfolioDetail.tsx
@@ -1,3 +1,4 @@
+import { ArrowBack } from "@mui/icons-material";
 import {
   Box,
   Typography,
@@ -7,62 +8,86 @@ import {
   List,
   ListItem,
   Link,
+  Button,
 } from "@mui/material";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 import { Fragment } from "react/jsx-runtime";
 import { camelToSentenceCase, isLinkInfo, isTechStack } from "src/utils/utils";
 import { PortfolioCard } from "types/global";
 
 const PortfolioDetail = () => {
   const portfolioDetail = useLoaderData() as PortfolioCard;
+  const navigate = useNavigate();
+
+  if (!portfolioDetail) {
+    return (
+      <Box className="mt-4">
+        <Typography variant="decoration">Not found</Typography>
+        <Typography component="h2" variant="h5">
+          Portfolio not found
+        </Typography>
+      </Box>
+    );
+  }
 
   return (
-    <Paper className="p-4 mb-4">
-      <Typography variant="decoration">Portfolio detail</Typography>
-      <Typography variant="h3">{portfolioDetail.title}</Typography>
-      <Divider />
-      <Typography className="my-2" component="h4" variant="h5">
-        {portfolioDetail.subheader}
-      </Typography>
-      <Typography variant="subtitle1">{portfolioDetail.affiliation}</Typography>
-      <Typography variant="h6">Description</Typography>
-      <Typography variant="body1">{portfolioDetail.description}</Typography>
-      <Typography variant="h6">Role</Typography>
-      <Typography variant="body1">{portfolioDetail.role}</Typography>
-      <Typography variant="h6">Technologies Used</Typography>
-      <Box sx={{ display: "flex", flexWrap: "wrap", gap: 1, marginTop: 2 }}>
-        {portfolioDetail.techStack.map((tech, index) => (
-          <Chip key={index} label={tech.name} variant="filled" />
-        ))}
-      </Box>
-      {["contributions", "shareholderDescription", "problemSolving"].map(
-        (key) => {
-          const data = portfolioDetail[key as keyof PortfolioCard];
-          if (!data) {
-            return null;
+    <>
+      <Button
+        className="mb-4"
+        startIcon={<ArrowBack />}
+        onClick={() => navigate(-1)}
+        variant="outlined"
+      >
+        Back to Portfolio
+      </Button>
+      <Paper className="p-4 mb-4">
+        <Typography variant="decoration">Portfolio detail</Typography>
+        <Typography variant="h3">{portfolioDetail.title}</Typography>
+        <Divider />
+        <Typography className="my-2" component="h4" variant="h5">
+          {portfolioDetail.subheader}
+        </Typography>
+        <Typography variant="subtitle1">
+          {portfolioDetail.affiliation}
+        </Typography>
+        <Typography variant="h6">Description</Typography>
+        <Typography variant="body1">{portfolioDetail.description}</Typography>
+        <Typography variant="h6">Role</Typography>
+        <Typography variant="body1">{portfolioDetail.role}</Typography>
+        <Typography variant="h6">Technologies Used</Typography>
+        <Box sx={{ display: "flex", flexWrap: "wrap", gap: 1, marginTop: 2 }}>
+          {portfolioDetail.techStack.map((tech, index) => (
+            <Chip key={index} label={tech.name} variant="filled" />
+          ))}
+        </Box>
+        {["contributions", "shareholderDescription", "problemSolving"].map(
+          (key) => {
+            const data = portfolioDetail[key as keyof PortfolioCard];
+            if (!data) {
+              return null;
+            }
+            return (
+              <Fragment key={key}>
+                <Typography variant="h6">{camelToSentenceCase(key)}</Typography>
+                {Array.isArray(data) ? (
+                  <List>
+                    {data.map((item, index) => (
+                      <ListItem key={index}>
+                        {isTechStack(item) ? <Chip label={item.name} /> : item}
+                      </ListItem>
+                    ))}
+                  </List>
+                ) : (
+                  <Typography variant="body1">
+                    {isLinkInfo(data) ? <Link {...data} /> : data}
+                  </Typography>
+                )}
+              </Fragment>
+            );
           }
-
-          return (
-            <Fragment key={key}>
-              <Typography variant="h6">{camelToSentenceCase(key)}</Typography>
-              {Array.isArray(data) ? (
-                <List>
-                  {data.map((item, index) => (
-                    <ListItem key={index}>
-                      {isTechStack(item) ? <Chip label={item.name} /> : item}
-                    </ListItem>
-                  ))}
-                </List>
-              ) : (
-                <Typography variant="body1">
-                  {isLinkInfo(data) ? <Link {...data} /> : data}
-                </Typography>
-              )}
-            </Fragment>
-          );
-        }
-      )}
-    </Paper>
+        )}
+      </Paper>
+    </>
   );
 };
 
