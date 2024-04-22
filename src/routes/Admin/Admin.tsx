8b11aa52fc27523587fc3cd649fb29f6af256cc4
Sapphyra Wiser <sapphybara@gmail.com>
2024-04-22 13:49:38 -0600

    add my own authentication management, remove amplify `withAuthenticator`

--- src/routes/Admin/Admin.tsx
+++ src/routes/Admin/Admin.tsx
@@ -2,26 +2,30 @@ import { useState } from "react";
 import { Box, Button, Typography } from "@mui/material";
 
 import { generateClient } from "aws-amplify/api";
-import { withAuthenticator } from "@aws-amplify/ui-react";
-import { type AuthUser } from "aws-amplify/auth";
-import { type UseAuthenticator } from "@aws-amplify/ui-react-core";
 
-import CreditCardTable from "src/components/CreditCardTable";
-import AddCreditCardDialog from "src/components/AddCreditCardDialog";
-import { useFetchCreditCards } from "src/hooks/hooks";
+import CreditCardTable from "@components/CreditCardTable";
+import AddCreditCardDialog from "@components/AddCreditCardDialog";
+import { useFetchCreditCards } from "@hooks/hooks";
+import { useAuth } from "@hooks/useAuth";
+import SignIn from "src/components/SignIn";
 
 const client = generateClient();
 
-interface AdminProps {
-  signOut?: UseAuthenticator["signOut"];
-  user?: AuthUser;
-}
-
-const Admin = ({ signOut, user: _user }: AdminProps) => {
+const Admin = () => {
   const [isAddCreditCardDialogOpen, setIsAddCreditCardDialogOpen] =
     useState(false);
-  const { creditCards, fetchCreditCards, error, loading } =
-    useFetchCreditCards(client);
+  const {
+    creditCards,
+    fetchCreditCards,
+    error: ccError,
+    loading: ccIsLoading,
+  } = useFetchCreditCards(client);
+  const {
+    error: _userError,
+    isLoading: userIsLoading,
+    user,
+    signOut,
+  } = useAuth();
 
   const changeAddCreditCardDialogOpen = (prevState?: boolean) => {
     if (prevState === undefined) {
@@ -35,7 +39,7 @@ const Admin = ({ signOut, user: _user }: AdminProps) => {
     changeAddCreditCardDialogOpen(false);
   };
 
-  if (error) {
+  if (ccError) {
     return <Typography>Error fetching credit cards</Typography>;
   }
 
@@ -43,34 +47,41 @@ const Admin = ({ signOut, user: _user }: AdminProps) => {
     <Box>
       <Typography variant="decoration">Manage</Typography>
       <Typography variant="h1">Admin</Typography>
-      <Box className="flex justify-between my-2">
-        <Typography component="h2" variant="h5">
-          Credit Cards
-        </Typography>
-        <Button
-          onClick={() => changeAddCreditCardDialogOpen()}
-          variant="contained"
-        >
-          Add Credit Card
-        </Button>
-        <AddCreditCardDialog
-          closeDialog={closeCreditCardDialog}
-          fetchCreditCards={fetchCreditCards}
-          open={isAddCreditCardDialogOpen}
-        />
-      </Box>
-      <CreditCardTable creditCards={creditCards} loading={loading} />
-      <Button
-        className="my-2"
-        color="warning"
-        onClick={signOut}
-        variant="contained"
-      >
-        Sign Out
-      </Button>
+      {userIsLoading ? (
+        <Typography paragraph>Loading...</Typography>
+      ) : !user ? (
+        <SignIn />
+      ) : (
+        <>
+          <Box className="flex justify-between my-2">
+            <Typography component="h2" variant="h5">
+              Credit Cards
+            </Typography>
+            <Button
+              onClick={() => changeAddCreditCardDialogOpen()}
+              variant="contained"
+            >
+              Add Credit Card
+            </Button>
+            <AddCreditCardDialog
+              closeDialog={closeCreditCardDialog}
+              fetchCreditCards={fetchCreditCards}
+              open={isAddCreditCardDialogOpen}
+            />
+          </Box>
+          <CreditCardTable creditCards={creditCards} loading={ccIsLoading} />
+          <Button
+            className="my-2"
+            color="warning"
+            onClick={signOut}
+            variant="contained"
+          >
+            Sign Out
+          </Button>
+        </>
+      )}
     </Box>
   );
 };
 
-const AdminWithAuthenticator = withAuthenticator(Admin);
-export default AdminWithAuthenticator;
+export default Admin;
