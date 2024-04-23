36a7ef0db1bfd85d4963b59cae3a3ca4bf966bf0
Sapphyra Wiser <sapphybara@gmail.com>
2024-04-22 19:47:45 -0600

    Refactor styling in CreditCardTable component

--- src/components/CreditCardTable.tsx
+++ src/components/CreditCardTable.tsx
@@ -1,5 +1,5 @@
 import { useMemo } from "react";
-import { Typography } from "@mui/material";
+import { Typography, darken, lighten, styled } from "@mui/material";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import dayjs from "dayjs";
 
@@ -23,6 +23,93 @@ const columnWidths: Record<
   paymentDate: 100,
   minimumPayment: 125,
 };
+
+const getBackgroundColor = (color: string, mode: string) =>
+  mode === "dark" ? darken(color, 0.7) : lighten(color, 0.7);
+
+const getHoverBackgroundColor = (color: string, mode: string) =>
+  mode === "dark" ? darken(color, 0.6) : lighten(color, 0.6);
+
+const getSelectedBackgroundColor = (color: string, mode: string) =>
+  mode === "dark" ? darken(color, 0.5) : lighten(color, 0.5);
+
+const getSelectedHoverBackgroundColor = (color: string, mode: string) =>
+  mode === "dark" ? darken(color, 0.4) : lighten(color, 0.4);
+
+const StyledDataGrid = styled(DataGrid)(({ theme }) => ({
+  "& .level-2": {
+    backgroundColor: getBackgroundColor(
+      theme.palette.grey[300],
+      theme.palette.mode
+    ),
+    "&:hover": {
+      backgroundColor: getHoverBackgroundColor(
+        theme.palette.grey[300],
+        theme.palette.mode
+      ),
+    },
+    "&.Mui-selected": {
+      backgroundColor: getSelectedBackgroundColor(
+        theme.palette.grey[300],
+        theme.palette.mode
+      ),
+      "&:hover": {
+        backgroundColor: getSelectedHoverBackgroundColor(
+          theme.palette.grey[300],
+          theme.palette.mode
+        ),
+      },
+    },
+  },
+  "& .level-3": {
+    backgroundColor: getBackgroundColor(
+      theme.palette.warning.main,
+      theme.palette.mode
+    ),
+    "&:hover": {
+      backgroundColor: getHoverBackgroundColor(
+        theme.palette.warning.main,
+        theme.palette.mode
+      ),
+    },
+    "&.Mui-selected": {
+      backgroundColor: getSelectedBackgroundColor(
+        theme.palette.warning.main,
+        theme.palette.mode
+      ),
+      "&:hover": {
+        backgroundColor: getSelectedHoverBackgroundColor(
+          theme.palette.warning.main,
+          theme.palette.mode
+        ),
+      },
+    },
+  },
+  "& .level-4": {
+    backgroundColor: getBackgroundColor(
+      theme.palette.error.main,
+      theme.palette.mode
+    ),
+    "&:hover": {
+      backgroundColor: getHoverBackgroundColor(
+        theme.palette.error.main,
+        theme.palette.mode
+      ),
+    },
+    "&.Mui-selected": {
+      backgroundColor: getSelectedBackgroundColor(
+        theme.palette.error.main,
+        theme.palette.mode
+      ),
+      "&:hover": {
+        backgroundColor: getSelectedHoverBackgroundColor(
+          theme.palette.error.main,
+          theme.palette.mode
+        ),
+      },
+    },
+  },
+}));
 
 const CreditCardTable = ({
   creditCards,
@@ -97,12 +184,22 @@ const CreditCardTable = ({
   }
 
   return (
-    <DataGrid
+    <StyledDataGrid
       autoHeight
       columns={columns}
       rows={creditCards}
       initialState={{
         sorting: { sortModel: [{ field: "score", sort: "desc" }] },
+      }}
+      getRowClassName={({ row: { score } }) => {
+        if (score < 0.3) {
+          return "level-1";
+        } else if (score < 0.6) {
+          return "level-2";
+        } else if (score < 0.8) {
+          return "level-3";
+        }
+        return "level-4";
       }}
     />
   );
