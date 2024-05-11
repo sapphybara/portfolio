ddd61e64c40c5cef1947da52967953ae5dfb9365
Sapphyra Wiser <sapphyra.wiser@gmail.com>
2024-05-10 22:30:06 -0600

    denote actions/loaders as `ActionFunction`s and `LoaderFunctions` rather than providing the argument declaration

--- src/routes/Admin/loader.ts
+++ src/routes/Admin/loader.ts
@@ -1,11 +1,11 @@
-import { LoaderFunctionArgs, redirect } from "react-router-dom";
+import { LoaderFunction, redirect } from "react-router-dom";
 import { isAuthenticated } from "@utils/AuthProvider";
 import { generateClient } from "aws-amplify/api";
 import { listCreditCards } from "src/graphql/queries";
 
 const client = generateClient();
 
-const loader = async ({ request }: LoaderFunctionArgs) => {
+const loader: LoaderFunction = async ({ request }) => {
   if (!(await isAuthenticated())) {
     const params = new URLSearchParams();
     params.set("from", new URL(request.url).pathname);
--- src/routes/logout/action.ts
+++ src/routes/logout/action.ts
@@ -1,7 +1,7 @@
-import { LoaderFunctionArgs, redirect } from "react-router-dom";
+import { ActionFunction, redirect } from "react-router-dom";
 import { signOut } from "@utils/AuthProvider";
 
-const action = async ({ request }: LoaderFunctionArgs) => {
+const action: ActionFunction = async ({ request }) => {
   const url = new URL(request.url);
   const params = new URLSearchParams(url.search);
   const from = params.get("from") || "/";
