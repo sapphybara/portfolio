59a5cb0213275053efc84f4198324efc9a47eeb3
Sapphyra Wiser <sapphyra.wiser@gmail.com>
2024-05-10 22:17:23 -0600

    add barrel file to implement better code splitting for logout route

---
+++ src/routes/logout/action.ts
@@ -0,0 +1,13 @@
+import { LoaderFunctionArgs, redirect } from "react-router-dom";
+import { signOut } from "@utils/AuthProvider";
+
+const action = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const params = new URLSearchParams(url.search);
+  const from = params.get("from") || "/";
+
+  await signOut();
+  return redirect(from);
+};
+
+export default action;
--- src/routes/logout/lazy.ts
+++ src/routes/logout/lazy.ts
@@ -1,11 +1,1 @@
-import { LoaderFunctionArgs, redirect } from "react-router-dom";
-import { signOut } from "@utils/AuthProvider";
-
-export const action = async ({ request }: LoaderFunctionArgs) => {
-  const url = new URL(request.url);
-  const params = new URLSearchParams(url.search);
-  const from = params.get("from") || "/";
-
-  await signOut();
-  return redirect(from);
-};
+export { default as action } from "./action";
