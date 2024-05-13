8d6d5ab092a1608e456554eab531164e2c4878e7
Sapphyra Wiser <sapphyra.wiser@gmail.com>
2024-05-13 14:22:54 -0600

    update the home component to include more information about me

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -1,6 +1,11 @@
 import { Box, Button, Stack, Typography, styled } from "@mui/material";
 import ResumeLinkWithTooltip from "@components/ResumeLinkWithTooltip";
 
+const ExtraHeightTypography = styled(Typography)(() => ({
+  lineHeight: "1.625",
+  maxWidth: "48rem",
+}));
+
 const Home = () => {
   const ProfilePicture = styled("img")((props) => ({
     backgroundColor: props.theme.palette.text.secondary,
@@ -35,10 +40,22 @@ const Home = () => {
           Designer
         </Typography>
       </Box>
-      <Typography className="leading-relaxed mt-8 mb-4" paragraph>
-        I build diverse front end and mobile applications, and my passion lies
-        in accessibility, in every domain.
-      </Typography>
+      <ExtraHeightTypography className="mt-8 mb-4" paragraph>
+        I am a dedicated Front End Developer and Designer based in Denver, CO,
+        with a passion for creating seamless and intuitive digital experiences.
+        My journey into the world of technology started with a fascination for
+        problem-solving, which led me to pursue a degree in Mathematics, with a
+        Certificate in Computer Science, from the University of Texas at Austin.
+      </ExtraHeightTypography>
+      <ExtraHeightTypography paragraph>
+        My professional journey has been marked by hands-on experience in
+        ideating, designing, and building interactive and user-centric
+        applications. I specialize in crafting pixel-perfect interfaces that not
+        only look visually appealing but also prioritize accessibility and
+        usability. This commitment to adhering to W3C standards ensures that
+        every user, regardless of ability, can navigate and interact with
+        digital products seamlessly.
+      </ExtraHeightTypography>
       <Stack className="mb-4" direction="row" flexWrap="wrap">
         <Button className="mr-4" href="/portfolio" variant="contained">
           Check out my work
