1fcb0619702d7fcf6c439f839afc616f15072e69
Sapphyra Wiser <sapphyra.wiser@gmail.com>
2024-05-13 15:17:49 -0600

    add highlight to the about me section of the home page to make the text pop

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -1,9 +1,22 @@
 import { Box, Button, Stack, Typography, styled } from "@mui/material";
 import ResumeLinkWithTooltip from "@components/ResumeLinkWithTooltip";
 
-const ExtraHeightTypography = styled(Typography)(() => ({
-  lineHeight: "1.625",
-  maxWidth: "48rem",
+const TypographyStack = styled(Stack)(({ theme }) => ({
+  margin: theme.spacing(2, 0),
+  "& .MuiTypography-root": {
+    lineHeight: "1.625",
+    maxWidth: "48rem",
+    "& .highlight": {
+      display: "inline-block",
+      fontWeight: "bold",
+      color: theme.palette.secondary.main,
+      transition: "transform 0.3s, padding 0.3s",
+      "&:hover": {
+        transform: "scale(1.08)",
+        padding: "0 0.2rem",
+      },
+    },
+  },
 }));
 
 const Home = () => {
@@ -40,22 +53,32 @@ const Home = () => {
           Designer
         </Typography>
       </Box>
-      <ExtraHeightTypography className="mt-8 mb-4" paragraph>
-        I am a dedicated Front End Developer and Designer based in Denver, CO,
-        with a passion for creating seamless and intuitive digital experiences.
-        My journey into the world of technology started with a fascination for
-        problem-solving, which led me to pursue a degree in Mathematics, with a
-        Certificate in Computer Science, from the University of Texas at Austin.
-      </ExtraHeightTypography>
-      <ExtraHeightTypography paragraph>
-        My professional journey has been marked by hands-on experience in
-        ideating, designing, and building interactive and user-centric
-        applications. I specialize in crafting pixel-perfect interfaces that not
-        only look visually appealing but also prioritize accessibility and
-        usability. This commitment to adhering to W3C standards ensures that
-        every user, regardless of ability, can navigate and interact with
-        digital products seamlessly.
-      </ExtraHeightTypography>
+      <TypographyStack gap={2}>
+        <Typography>
+          Welcome to my corner of the digital world! I'm a dedicated Front End
+          Developer and Designer based in Denver, CO, with a passion for
+          creating&nbsp;
+          <Box className="highlight" component="span">
+            fluid
+          </Box>
+          &nbsp;and intuitive digital experiences. My journey into the world of
+          technology started with a fascination for problem-solving, which led
+          me to pursue a degree in Mathematics, with a Certificate in Computer
+          Science, from the University of Texas at Austin.
+        </Typography>
+        <Typography>
+          My professional journey has been marked by hands-on experience in
+          ideating, designing, and building interactive and user-centric
+          applications. I specialize in crafting pixel-perfect interfaces that
+          not only look visually appealing but also prioritize accessibility and
+          usability. This commitment to adhering to W3C standards ensures that
+          every user, regardless of ability, can navigate and interact with
+          digital products&nbsp;
+          <Box className="highlight" component="span">
+            seamlessly.
+          </Box>
+        </Typography>
+      </TypographyStack>
       <Stack className="mb-4" direction="row" flexWrap="wrap">
         <Button className="mr-4" href="/portfolio" variant="contained">
           Check out my work
