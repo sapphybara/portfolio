8bc506d02e9a106d74ce4f67e61b96c37ab21f38
Sapphyra Wiser <sapphyra.wiser@gmail.com>
2024-05-14 15:06:05 -0600

    add `find`, `replace` objects to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import checker from "vite-plugin-checker";
-import { resolve } from "path";
+import { fileURLToPath } from "url";
 
-console.log(resolve(__dirname, "src"));
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -17,16 +16,45 @@ export default defineConfig({
     }),
   ],
   resolve: {
-    alias: {
-      "@": resolve(__dirname, "src"),
-      "@components": resolve(__dirname, "src/components"),
-      "@context": resolve(__dirname, "src/context"),
-      "@routes": resolve(__dirname, "src/routes"),
-      "@theme": resolve(__dirname, "src/theme"),
-      "@utils": resolve(__dirname, "src/utils"),
-      "@hooks": resolve(__dirname, "src/hooks"),
-      "@graphql": resolve(__dirname, "src/graphql"),
-      "@assets": resolve(__dirname, "src/assets"),
-    },
+    alias: [
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("./src", import.meta.url)),
+      },
+      {
+        find: "@components",
+        replacement: fileURLToPath(
+          new URL("./src/components", import.meta.url)
+        ),
+      },
+      {
+        find: "@context",
+        replacement: fileURLToPath(new URL("./src/context", import.meta.url)),
+      },
+      {
+        find: "@routes",
+        replacement: fileURLToPath(new URL("./src/routes", import.meta.url)),
+      },
+      {
+        find: "@theme",
+        replacement: fileURLToPath(new URL("./src/theme", import.meta.url)),
+      },
+      {
+        find: "@utils",
+        replacement: fileURLToPath(new URL("./src/utils", import.meta.url)),
+      },
+      {
+        find: "@hooks",
+        replacement: fileURLToPath(new URL("./src/hooks", import.meta.url)),
+      },
+      {
+        find: "@graphql",
+        replacement: fileURLToPath(new URL("./src/graphql", import.meta.url)),
+      },
+      {
+        find: "@assets",
+        replacement: fileURLToPath(new URL("./src/assets", import.meta.url)),
+      },
+    ],
   },
 });
