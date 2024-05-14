97234876cd86786ef0390e7bc8aabb5fba32ebb8
Sapphyra Wiser <sapphyra.wiser@gmail.com>
2024-05-14 13:54:18 -0600

    add congnito user pools so we can privatize the api

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -2,8 +2,7 @@ export type AmplifyDependentResourcesAttributes = {
   "api": {
     "portfolio": {
       "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
+      "GraphQLAPIIdOutput": "string"
     }
   },
   "auth": {
