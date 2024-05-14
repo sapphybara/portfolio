dced58e75680b5d357d06fa8a2c79135306bb2a4
Sapphyra Wiser <sapphyra.wiser@gmail.com>
2024-05-14 15:15:59 -0600

    use `resolve` inside vite alias replacement

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import checker from "vite-plugin-checker";
-import { fileURLToPath } from "url";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,44 +17,18 @@ export default defineConfig({
   ],
   resolve: {
     alias: [
-      {
-        find: "@",
-        replacement: fileURLToPath(new URL("./src", import.meta.url)),
-      },
+      { find: "@", replacement: resolve(__dirname, "./src") },
       {
         find: "@components",
-        replacement: fileURLToPath(
-          new URL("./src/components", import.meta.url)
-        ),
+        replacement: resolve(__dirname, "./src/components"),
       },
-      {
-        find: "@context",
-        replacement: fileURLToPath(new URL("./src/context", import.meta.url)),
-      },
-      {
-        find: "@routes",
-        replacement: fileURLToPath(new URL("./src/routes", import.meta.url)),
-      },
-      {
-        find: "@theme",
-        replacement: fileURLToPath(new URL("./src/theme", import.meta.url)),
-      },
-      {
-        find: "@utils",
-        replacement: fileURLToPath(new URL("./src/utils", import.meta.url)),
-      },
-      {
-        find: "@hooks",
-        replacement: fileURLToPath(new URL("./src/hooks", import.meta.url)),
-      },
-      {
-        find: "@graphql",
-        replacement: fileURLToPath(new URL("./src/graphql", import.meta.url)),
-      },
-      {
-        find: "@assets",
-        replacement: fileURLToPath(new URL("./src/assets", import.meta.url)),
-      },
+      { find: "@context", replacement: resolve(__dirname, "./src/context") },
+      { find: "@routes", replacement: resolve(__dirname, "./src/routes") },
+      { find: "@theme", replacement: resolve(__dirname, "./src/theme") },
+      { find: "@utils", replacement: resolve(__dirname, "./src/utils") },
+      { find: "@hooks", replacement: resolve(__dirname, "./src/hooks") },
+      { find: "@graphql", replacement: resolve(__dirname, "./src/graphql") },
+      { find: "@assets", replacement: resolve(__dirname, "./src/assets") },
     ],
   },
 });
