fcbb82fb9271017fc278d5e60b0c42aa5db41ee5
Sapphyra Wiser <sapphyra.wiser@gmail.com>
2024-05-14 14:56:58 -0600

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import checker from "vite-plugin-checker";
 import { resolve } from "path";
 
+console.log(resolve(__dirname, "src"));
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
