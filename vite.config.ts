bf6873585c1a0f3e3775449720755820644414a5
Sapphyra Wiser <sapphyra.wiser@gmail.com>
2024-05-14 16:18:32 -0600

    sheepishly revert vite config to status before finding folder rename bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,19 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import checker from "vite-plugin-checker";
-import { resolve } from "path";
+import { join, resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    checker({
-      typescript: true,
-      eslint: {
-        // lint .ts and .tsx
-        lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
-      },
-    }),
-  ],
   resolve: {
-    alias: [
-      { find: "@", replacement: resolve(__dirname, "./src") },
-      {
-        find: "@components",
-        replacement: resolve(__dirname, "./src/components"),
-      },
-      { find: "@context", replacement: resolve(__dirname, "./src/context") },
-      { find: "@routes", replacement: resolve(__dirname, "./src/routes") },
-      { find: "@theme", replacement: resolve(__dirname, "./src/theme") },
-      { find: "@utils", replacement: resolve(__dirname, "./src/utils") },
-      { find: "@hooks", replacement: resolve(__dirname, "./src/hooks") },
-      { find: "@graphql", replacement: resolve(__dirname, "./src/graphql") },
-      { find: "@assets", replacement: resolve(__dirname, "./src/assets") },
-    ],
+    alias: {
+      "@": resolve(join(__dirname, "src")),
+      "@components": resolve(join(__dirname, "src/components")),
+      "@context": resolve(join(__dirname, "src/context")),
+      "@routes": resolve(join(__dirname, "src/routes")),
+      "@theme": resolve(join(__dirname, "src/theme")),
+      "@utils": resolve(join(__dirname, "src/utils")),
+      "@hooks": resolve(join(__dirname, "src/hooks")),
+      "@graphql": resolve(join(__dirname, "src/graphql")),
+      "@assets": resolve(join(__dirname, "src/assets")),
+    },
   },
 });
