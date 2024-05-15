b625df9585393c7e8d198e8ae21d13a4158a33aa
Sapphyra Wiser <sapphyra.wiser@gmail.com>
2024-05-15 14:33:57 -0600

    add typscript/eslint back to vite config plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import checker from "vite-plugin-checker";
 import { join, resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+      eslint: {
+        // lint .ts and .tsx
+        lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": resolve(join(__dirname, "src")),
