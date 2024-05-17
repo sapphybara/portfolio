7991e7fcfc8d44ce03f691765cbb740af7d6f10b
Sapphyra Wiser <wswiser101@utexas.edu>
2024-05-17 14:47:39 -0600

    WIP first attempt to make the image carousel more responsive

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -1,4 +1,12 @@
-import { Box, Card, CardMedia, Fab, Slide, styled } from "@mui/material";
+import {
+  Box,
+  Card,
+  CardMedia,
+  Fab,
+  Slide,
+  Typography,
+  styled,
+} from "@mui/material";
 import { ArrowBack, ArrowForward } from "@mui/icons-material";
 import { useState } from "react";
 import { type PortfolioItemImage } from "types/global";
@@ -11,22 +19,41 @@ const AbsoluteFab = styled(Fab)({
   },
 });
 
+const TextOverlay = styled(Typography)(({ theme }) => ({
+  position: "absolute",
+  left: "50%",
+  bottom: theme.spacing(3),
+  transform: "translateX(-50%)",
+  color: theme.palette.common.white,
+  backgroundColor: "rgba(0, 0, 0, 0.65)",
+  borderRadius: theme.spacing(1),
+  padding: theme.spacing(0.5),
+  textAlign: "center",
+}));
+
 const ImageCarousel = ({ images }: { images: PortfolioItemImage[] }) => {
   const [activeIndex, setActiveIndex] = useState(0);
   const [slideDirection, setSlideDirection] = useState<
     "left" | "right" | "up" | "down"
   >("left");
+  const [loadedImages, setLoadedImages] = useState<number[]>([0]);
 
   const nextSlide = () => {
-    setActiveIndex((prevIndex) => (prevIndex + 1) % images.length);
-    setSlideDirection("left");
+    const newIndex = (activeIndex + 1) % images.length;
+    setActiveIndex(newIndex);
+    setSlideDirection("right");
+    if (!loadedImages.includes(newIndex)) {
+      setLoadedImages((prevLoadedImages) => [...prevLoadedImages, newIndex]);
+    }
   };
 
   const prevSlide = () => {
-    setActiveIndex(
-      (prevIndex) => (prevIndex - 1 + images.length) % images.length
-    );
-    setSlideDirection("right");
+    const newIndex = (activeIndex - 1 + images.length) % images.length;
+    setActiveIndex(newIndex);
+    setSlideDirection("left");
+    if (!loadedImages.includes(newIndex)) {
+      setLoadedImages((prevLoadedImages) => [...prevLoadedImages, newIndex]);
+    }
   };
 
   return (
@@ -39,11 +66,23 @@ const ImageCarousel = ({ images }: { images: PortfolioItemImage[] }) => {
       >
         <ArrowBack />
       </AbsoluteFab>
-      <Slide direction={slideDirection} in={true} key={activeIndex}>
-        <Card>
-          <CardMedia component="img" {...images[activeIndex]} />
-        </Card>
-      </Slide>
+      {images.map((image, index) => (
+        <Slide
+          direction={slideDirection}
+          in={activeIndex === index}
+          key={index}
+          unmountOnExit
+        >
+          <Card>
+            {loadedImages.includes(index) ? (
+              <>
+                <CardMedia component="img" {...image} />
+                <TextOverlay>{image.description}</TextOverlay>
+              </>
+            ) : null}
+          </Card>
+        </Slide>
+      ))}
       <AbsoluteFab
         aria-label="next slide"
         className="right-2.5"
