29fda9a0e697a1c3ee28fc2f53ecff41ac65ed10
Sapphyra Wiser <sapphyra.wiser@gmail.com>
2024-05-31 11:18:33 -0600

    refactor: Improve logo animation and interaction, add a little flip on rotate

--- src/components/ResponsiveLogo.tsx
+++ src/components/ResponsiveLogo.tsx
@@ -1,53 +1,34 @@
-import { IconButton, keyframes, styled } from "@mui/material";
+import { IconButton, styled } from "@mui/material";
 import { useContext, useState } from "react";
 import { ThemeModeContext } from "@context/ThemeModeContext";
 
-const RotateS = keyframes`
-  from {
-    transform: rotate(270deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-`;
-
-const RotateW = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(270deg);
-  }
-`;
-
-const Logo = styled("img")<{ initial: "s" | "w" }>(({ initial }) => ({
+const Logo = styled("img")(() => ({
   aspectRatio: "1",
   height: 35,
-  animation: `${initial === "s" ? RotateS : RotateW} 400ms forwards`,
+  transition: "transform 400ms ease-out",
 }));
 
 const ResponsiveLogo = () => {
   const [logoInitial, setLogoInitial] = useState<"s" | "w">("s");
+  const [rotation, setRotation] = useState(0);
+  const [flip, setFlip] = useState<1 | -1>(1);
+
   const { isDarkMode } = useContext(ThemeModeContext);
 
   const handleLogoClick = () => {
-    setLogoInitial((prevState) => {
-      switch (prevState) {
-        case "s":
-          return "w";
-        case "w":
-        default:
-          return "s";
-      }
-    });
+    setLogoInitial((prevState) => (prevState === "s" ? "w" : "s"));
+    setRotation((prevRotation) => prevRotation + 180);
+    setFlip((prevFlip) => (prevFlip * -1) as 1 | -1);
   };
 
   return (
     <IconButton className="p-0" onClick={handleLogoClick}>
       <Logo
         alt="logo"
-        initial={logoInitial}
         src={`/logo-${logoInitial}-${isDarkMode ? "dark" : "light"}-mode.png`}
+        sx={{
+          transform: `rotate(${rotation}deg) scaleY(${flip})`,
+        }}
       />
     </IconButton>
   );
