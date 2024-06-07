52fc5ff1b5cc8e01eaad3a57e76195d23dc38c2c
Sapphyra Wiser <sapphyra.wiser@gmail.com>
2024-06-07 12:03:14 -0600

    WIP: create a new contact form on submit on "/contact"
    - still need to update the Contact page after successful email send
    - relates to #5

--- src/routes/contact/action.ts
+++ src/routes/contact/action.ts
@@ -1,12 +1,28 @@
+import { createContactForm } from "@/graphql/mutations";
+import { generateClient } from "aws-amplify/api";
 import { ActionFunction } from "react-router-dom";
-import { ContactFormField } from "types/global";
+import { CreateContactFormInput } from "@/API";
+
+const client = generateClient();
 
 const action: ActionFunction = async ({ request }) => {
   const formState = Object.fromEntries(await request.formData()) as {
-    [key in ContactFormField["key"]]: string;
+    [key in keyof Exclude<CreateContactFormInput, "id">]: string;
   };
-  console.log(formState);
-  return null;
+
+  try {
+    await client.graphql({
+      query: createContactForm,
+      variables: {
+        input: formState,
+      },
+      authMode: "iam",
+    });
+  } catch (err) {
+    console.error("error creating new contact form:", err);
+    return { status: (err as Error).message };
+  }
+  return { status: "Form submitted successfully!" };
 };
 
 export default action;
