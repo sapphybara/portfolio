6b6dab6e6fdb9b43dd0c234e7eaa87179f650ce9
Sapphyra Wiser <sapphyra.wiser@gmail.com>
2024-06-07 11:15:47 -0600

    WIP: add function to send an email when contacted

--- amplify/backend/function/contactemailtrigger/src/index.js
+++ amplify/backend/function/contactemailtrigger/src/index.js
@@ -1,14 +1,39 @@
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+const { SESClient, SendEmailCommand } = require("@aws-sdk/client-ses");
+const ses = new SESClient({ region: "us-west-1" });
 
-/**
- * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
- */
-exports.handler = event => {
-  console.log(`EVENT: ${JSON.stringify(event)}`);
-  for (const record of event.Records) {
-    console.log(record.eventID);
-    console.log(record.eventName);
-    console.log('DynamoDB Record: %j', record.dynamodb);
+// eslint-disable-next-line no-undef
+exports.handler = async (event) => {
+  const { toAddress } = event;
+
+  const command = new SendEmailCommand({
+    Destination: {
+      ToAddresses: [toAddress],
+    },
+    Message: {
+      Body: {
+        Text: { Data: "This is a testing email from the lambda function" },
+      },
+
+      Subject: { Data: "Test Email" },
+    },
+    Source: "sapphyra.wiser@gmail.com",
+  });
+
+  try {
+    let response = await ses.send(command);
+    // process data.
+    console.log(response);
+    return response;
   }
-  return Promise.resolve('Successfully processed DynamoDB record');
+  catch (error) {
+    // todo error handling.
+    console.log("error!", error)
+  }
+  finally {
+// todo finally.
+  }
 };
