4d67d861e60a31c70e8fbf8523addce324b8fcc4
Sapphyra Wiser <sapphyra.wiser@gmail.com>
2024-06-13 11:03:08 -0600

    Chore: change authMode to "userPool" if the user is logged in for the contact form
    This one will actually fix #12

--- src/routes/contact/action.ts
+++ src/routes/contact/action.ts
@@ -2,6 +2,7 @@ import { createContactForm } from "@/graphql/mutations";
 import { generateClient } from "aws-amplify/api";
 import { ActionFunction } from "react-router-dom";
 import { CreateContactFormInput } from "@/API";
+import { username } from "@utils/AuthProvider";
 
 const client = generateClient();
 
@@ -11,12 +12,13 @@ const action: ActionFunction = async ({ request }) => {
   };
 
   try {
+    const user = await username();
     await client.graphql({
       query: createContactForm,
       variables: {
         input: formState,
       },
-      authMode: "iam",
+      authMode: user ? "userPool" : "iam",
     });
   } catch (err) {
     console.error("error creating new contact form:", err);
