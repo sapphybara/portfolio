3ab25970b3bc62a926c569df58f618aabbd0e2ea
Sapphyra Wiser <sapphyra.wiser@gmail.com>
2025-06-01 13:15:03 -0600

    fix: overhaul PortfolioCards - make use of `href` prop - update styling to allow card to work outside of the TabPanel interface of the old Home design

--- src/components/portfolio/PortfolioCard.tsx
+++ src/components/portfolio/PortfolioCard.tsx
@@ -10,12 +10,14 @@ import {
   Typography,
   styled,
 } from "@mui/material";
-import { useNavigate } from "react-router-dom";
 import { PortfolioItem } from "types/global";
 import { GitHub, LinkOutlined } from "@mui/icons-material";
 import TagChips from "@components/TagChips";
 
-const PortfolioWrapper = styled(Card)(({ theme }) => ({
+const PortfolioCardWrapper = styled(Card)(({ theme }) => ({
+  height: "100%",
+  display: "flex",
+  flexDirection: "column",
   ...(theme.palette.mode === "dark" && {
     "&:hover": {
       boxShadow: theme.shadows[8],
@@ -30,58 +32,51 @@ const PortfolioCard = ({
   techStack,
   links,
   shortDescription,
-}: PortfolioItem) => {
-  const navigate = useNavigate();
-
-  return (
-    <PortfolioWrapper key={title} id={title.replace(/\s/g, "-")}>
-      <CardActionArea
-        className="items-start flex flex-1"
-        onClick={() => navigate(`/portfolio/${id}`)}
-      >
-        <CardContent className="pt-0">
-          <CardHeader
-            title={title}
-            titleTypographyProps={{ variant: "h3", component: "h3" }}
-          />
-          <TagChips items={techStack} />
-          <Typography className="mt-4 mx-4" paragraph>
-            {shortDescription}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardContent className="pt-0">
-        <Stack
-          alignItems="center"
-          direction="row"
-          gap={1}
-          justifyContent="space-between"
-          component={CardActions}
-        >
-          <Typography className="mb-0" color="text.secondary" paragraph>
-            Project on behalf of: {affiliation}
-          </Typography>
-          <Stack direction="row" gap={1}>
-            {links?.map(({ type, ...link }, index) => (
-              <Tooltip
-                title={type === "code" ? "View Code" : "View Project"}
-                key={index}
-              >
-                <IconButton
-                  aria-label={type === "code" ? "View Code" : "View Project"}
-                  rel="noopener noreferrer"
-                  target="_blank"
-                  {...link}
-                >
-                  {type === "code" ? <GitHub /> : <LinkOutlined />}
-                </IconButton>
-              </Tooltip>
-            ))}
-          </Stack>
-        </Stack>
+}: PortfolioItem) => (
+  <PortfolioCardWrapper key={title} id={title.replace(/\s/g, "-")}>
+    <CardActionArea className="items-start flex-1" href={`/portfolio/${id}`}>
+      <CardContent className="pt-0 flex flex-col">
+        <CardHeader
+          title={title}
+          titleTypographyProps={{ variant: "h3", component: "h3" }}
+        />
+        <TagChips items={techStack} />
+        <Typography className="mt-4 mx-4" paragraph>
+          {shortDescription}
+        </Typography>
       </CardContent>
-    </PortfolioWrapper>
-  );
-};
+    </CardActionArea>
+    <CardContent className="pt-0">
+      <Stack
+        alignItems="center"
+        direction="row"
+        gap={1}
+        justifyContent="space-between"
+        component={CardActions}
+      >
+        <Typography className="mb-0" color="text.secondary" paragraph>
+          Project on behalf of: {affiliation}
+        </Typography>
+        <Stack direction="row" gap={1}>
+          {links?.map(({ type, ...link }, index) => (
+            <Tooltip
+              title={type === "code" ? "View Code" : "View Project"}
+              key={index}
+            >
+              <IconButton
+                aria-label={type === "code" ? "View Code" : "View Project"}
+                rel="noopener noreferrer"
+                target="_blank"
+                {...link}
+              >
+                {type === "code" ? <GitHub /> : <LinkOutlined />}
+              </IconButton>
+            </Tooltip>
+          ))}
+        </Stack>
+      </Stack>
+    </CardContent>
+  </PortfolioCardWrapper>
+);
 
 export default PortfolioCard;
