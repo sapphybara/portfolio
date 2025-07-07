db2f36801ce2d25955881e3521b55693863b4e66
Sapphyra Wiser <sapphyra.wiser@gmail.com>
2025-07-07 11:36:33 -0600

    feat: replace custom carousel implementation with `embla` - this is WIP as we'll need to still add captions and fullscreen

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -1,81 +1,144 @@
-import {
-  Box,
-  Card,
-  CardContent,
-  CardMedia,
-  Fab,
-  Typography,
-  styled,
-} from "@mui/material";
-import { ArrowBack, ArrowForward } from "@mui/icons-material";
-import { useState } from "react";
-import { type PortfolioItemImage } from "types/global";
+import { useCallback, useEffect, useState } from "react";
+import { ChevronLeft, ChevronRight } from "@mui/icons-material";
+import { IconButton, Stack, styled } from "@mui/material";
+import useEmblaCarousel, {
+  type UseEmblaCarouselType,
+} from "embla-carousel-react";
+import type { PortfolioItemImage } from "types/global";
 
-const AbsoluteFab = styled(Fab)({
-  position: "absolute",
-  top: "50%",
-  "& .MuiSvgIcon-root": {
-    color: "black",
+const EmblaContainer = styled("div")(({ theme }) => ({
+  "&.embla": {
+    overflow: "hidden",
   },
-});
-
-const TextOverlay = styled(Typography)(({ theme }) => ({
-  position: "absolute",
-  left: "50%",
-  bottom: theme.spacing(3),
-  transform: "translateX(-50%)",
-  color: theme.palette.common.white,
-  backgroundColor: "rgba(0, 0, 0, 0.65)",
-  borderRadius: theme.spacing(1),
-  padding: theme.spacing(0.5),
-  textAlign: "center",
-}));
-
-const CardImageContent = styled(CardContent)(() => ({
-  padding: 0,
-  "& .MuiCardMedia-root": {
-    objectFit: "unset",
+  ".embla__container": {
+    display: "flex",
+    width: "100%",
+  },
+  ".embla__slide": {
+    "--embla-slide-width": "90%",
+    flex: "0 0 var(--embla-slide-width)",
+    minWidth: 0,
+    marginRight: "1em",
+  },
+  ".embla__dots": {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    gap: "8px",
+    margin: 0,
+    flex: 1,
+  },
+  ".embla__dot": {
+    "--embla_dot": "12px",
+    width: "var(--embla_dot)",
+    height: "var(--embla_dot)",
+    borderRadius: "calc(var(--embla_dot) / 2)",
+    backgroundColor: theme.palette.grey[700],
+    border: "none",
+    cursor: "pointer",
+    transition: "background-color 0.2s ease",
+    "&:hover": {
+      backgroundColor: theme.palette.grey[500],
+    },
+    "&.embla__dot--selected": {
+      backgroundColor: theme.palette.grey[200],
+    },
   },
 }));
 
 const ImageCarousel = ({ images }: { images: PortfolioItemImage[] }) => {
-  const [activeIndex, setActiveIndex] = useState(0);
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [scrollSnaps, setScrollSnaps] = useState<number[]>([]);
 
-  const nextSlide = () => {
-    setActiveIndex((activeIndex + 1) % images.length);
-  };
+  const scrollTo = useCallback(
+    (index: number) => emblaApi && emblaApi.scrollTo(index),
+    [emblaApi]
+  );
 
-  const prevSlide = () => {
-    setActiveIndex((activeIndex - 1 + images.length) % images.length);
-  };
+  const onInit = useCallback((emblaApi: UseEmblaCarouselType[1]) => {
+    if (emblaApi) {
+      setScrollSnaps(emblaApi.scrollSnapList());
+    }
+  }, []);
 
-  const { description, ...image } = images[activeIndex];
+  const onSelect = useCallback((emblaApi: UseEmblaCarouselType[1]) => {
+    if (emblaApi) {
+      setSelectedIndex(emblaApi.selectedScrollSnap());
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!emblaApi) {
+      return;
+    }
+
+    onInit(emblaApi);
+    onSelect(emblaApi);
+    emblaApi.on("reInit", onInit);
+    emblaApi.on("select", onSelect);
+  }, [emblaApi, onInit, onSelect]);
+
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) {
+      emblaApi.scrollPrev();
+    }
+  }, [emblaApi]);
+
+  const scrollNext = useCallback(() => {
+    if (emblaApi) {
+      emblaApi.scrollNext();
+    }
+  }, [emblaApi]);
 
   return (
-    <Box className="relative">
-      <AbsoluteFab
-        aria-label="previous slide"
-        className="left-2.5"
-        color="secondary"
-        onClick={prevSlide}
-      >
-        <ArrowBack />
-      </AbsoluteFab>
-      <Card>
-        <CardImageContent>
-          <CardMedia component="img" {...image} />
-          <TextOverlay>{description}</TextOverlay>
-        </CardImageContent>
-      </Card>
-      <AbsoluteFab
-        aria-label="next slide"
-        className="right-2.5"
-        color="secondary"
-        onClick={nextSlide}
-      >
-        <ArrowForward />
-      </AbsoluteFab>
-    </Box>
+    <EmblaContainer className="embla">
+      <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla__container">
+          {images.map((image, i) => (
+            <div className="embla__slide" key={image.src}>
+              <img
+                src={image.src}
+                alt={image.alt}
+                width="100%"
+                loading={i === 0 ? "eager" : "lazy"}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+      <Stack direction="row" spacing={0.5}>
+        <IconButton
+          aria-label="Previous Slide"
+          className="embla__prev"
+          onClick={scrollPrev}
+          sx={{ bgcolor: (theme) => theme.palette.grey[800] }}
+        >
+          <ChevronLeft />
+        </IconButton>
+        <IconButton
+          aria-label="Next Slide"
+          className="embla__next"
+          onClick={scrollNext}
+          sx={{ bgcolor: (theme) => theme.palette.grey[800] }}
+        >
+          <ChevronRight />
+        </IconButton>
+        <div className="embla__dots">
+          {scrollSnaps.map((_, index) => (
+            <button
+              key={index}
+              className={`embla__dot ${
+                index === selectedIndex ? "embla__dot--selected" : ""
+              }`}
+              type="button"
+              onClick={() => scrollTo(index)}
+              aria-label={`Go to slide ${index + 1}`}
+            />
+          ))}
+        </div>
+      </Stack>
+    </EmblaContainer>
   );
 };
 
